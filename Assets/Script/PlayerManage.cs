8053cd85412a39d79ebe5ff3be4895bdbedba7f4
AAF-07 <firmansyahaziz38@gmail.com>
2026-08-25 17:15:37 +0700

    make player bounce when touch platform

---
+++ Assets/Script/PlayerManage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManage : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private float moveX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveX * speed;
+        rb.velocity = velocity;
+    }
+}
