3acc317948fd859f04f8826715a6dab081990caf
Muhammad Harun Ikhsan <muhasan713@gmail.com>
2026-08-28 23:22:23 +0700

    add pause logic

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -6,19 +6,60 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
 
-    public static bool GameIsPaused = true;
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
 
-
-    
+    void Start()
+    {
+        if (pauseMenuUI != null)
+        {
+            pauseMenuUI.SetActive(false);
+        }
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameManager.gameover) return;
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
     public void QuitGame()
     {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
         SceneManager.LoadScene("Main Menu");
     }
 }
