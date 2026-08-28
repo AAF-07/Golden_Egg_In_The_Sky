14c12be3c80a723769c8664150508cc846e45cb5
Muhammad Harun Ikhsan <muhasan713@gmail.com>
2026-08-28 16:16:21 +0700

    change function in eggController

--- Assets/Script/eggController.cs
+++ Assets/Script/eggController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class eggController : MonoBehaviour
 {
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
