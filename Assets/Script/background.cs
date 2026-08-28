ec358db7f7eb2c1469c3d10a1608ec7ca22b4888
AAF-07 <firmansyahaziz38@gmail.com>
2026-08-28 22:34:33 +0700

    generate bg'

--- Assets/Script/background.cs
+++ Assets/Script/background.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class background : MonoBehaviour
 {
+    public GameObject bg;
+    public float loop = 10;
+    public float heightbg = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 spawnPosition = Vector3.zero;
+        for(int i = 0; i < loop; i++)
+        {
+            spawnPosition.y += heightbg;
+            Instantiate(bg, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
