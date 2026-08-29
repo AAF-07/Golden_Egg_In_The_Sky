a475ecf2c5b2a6fbcbba6d38ca6ef1fdc3916e32
AAF-07 <firmansyahaziz38@gmail.com>
2026-08-29 14:12:21 +0700

    fix sound

--- Assets/Script/eggController.cs
+++ Assets/Script/eggController.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class eggController : MonoBehaviour
 {
+    private Collider2D myCollider;
+    private SpriteRenderer myRenderer;
+
+    void Start()
+   {
+        myCollider = GetComponent<Collider2D>();
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            FindAnyObjectByType<ScoreManager>().AddScore(10);
-            AudioManager.Instance.playcollect();
-            Destroy(gameObject);
+            ScoreManager scoreManager = FindAnyObjectByType<ScoreManager>();
+            if (scoreManager != null)
+            {
+                scoreManager.AddScore(10);
+            }
+
             Debug.Log("player hit egg");
+
+            if (AudioManager.Instance != null)
+            {
+                AudioManager.Instance.playcollect();
+            }
+
+            if (myRenderer != null) myRenderer.enabled = false;
+            if (myCollider != null) myCollider.enabled = false;
+            Destroy(gameObject, 1.0f);
         }
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
